2979579abeb3151c13df15f0589401836744ca93
XLEPROGRAMMEUR <155170880+XLEPROGRAMMEUR@users.noreply.github.com>
2024-05-15 02:19:31 +0200

    Update mainmapdata.js

--- js/mainmapdata.js
+++ js/mainmapdata.js
@@ -1,74 +1,223 @@
+// stores the reference to the XMLHttpRequest object
+var xmlHttp = createXmlHttpRequestObject();
+// retrieves the XMLHttpRequest object
+function createXmlHttpRequestObject() {
+    // stores the reference to the XMLHttpRequest object
+    var xmlHttp;
+    // if running Internet Explorer 6 or older
+    if (window.ActiveXObject) {
+        try {
+            xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        catch (e) {
+            xmlHttp = false;
+        }
+    }
+    // if running Mozilla or other browsers
+    else {
+        try {
+            xmlHttp = new XMLHttpRequest();
+        }
+        catch (e) {
+            xmlHttp = false;
+        }
+    }
+    // return the created object or display an error message
+    if (!xmlHttp)
+        alert("Error creating the XMLHttpRequest object.");
+    else
+        return xmlHttp;
+}
+// make asynchronous HTTP request using the XMLHttpRequest object 
+function process() {
 
-var position = JSON.parse(window.localStorage.getItem("position"));
-
-var long = position.long;
-var lat = position.lat;
-
-var map = L.map('map').setView([long, lat], 18); // Centre la carte à une certaine latitude et longitude, avec un certain niveau de zoom
-
-map.setView([lat, long, 100]);
-
-
-//code qui faisais apparaitre le marker à chaque click
-/* map.on('click', (e) => {          
-    marker.setLatLng(e.latlng);
-});  */
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { // Utilisez un service de tuiles, comme OpenStreetMap
+    // proceed only if the xmlHttp object isn't busy
+    if (xmlHttp.readyState == 4 || xmlHttp.readyState == 0) {
+        // retrieve the names typed by the user on the form
+        names = encodeURIComponent(
+            lat);
+        name = encodeURIComponent(
+            lon);
+        // execute the quickstart.php page from the server
+        xmlHttp.open("GET", "./madatas.php?names=" + names+"&name="+name, true);
+        
+        // define the method to handle server responses
+        xmlHttp.onreadystatechange = handleServerResponse;
+        // make the server request
+        xmlHttp.send(null);
+    }
+    else
+        // if the connection is busy, try again after one second 
+        setTimeout('process()', 1000);
+    window.location = './adddata.php';
+}
+// callback function executed when a message is received from the 
+//server
+function handleServerResponse() {
+    // move forward only if the transaction has completed
+    if (xmlHttp.readyState == 4) {
+        // status of 200 indicates the transaction completed 
+        //successfully
+        if (xmlHttp.status == 200) {
+            // extract the XML retrieved from the server
+            xmlResponse = xmlHttp.responseXML;
+            // obtain the document element (the root element) of the XML 
+            //structure
+            xmlDocumentElement = xmlResponse.documentElement;
+            // get the text message, which is in the first child of
+            // the the document element
+            helloMessage = xmlDocumentElement.firstChild.data;
+            // display the data received from the server
+            alert(helloMessage)
+            // restart sequence
+            setTimeout('process()', 0);
+        }
+        // a HTTP status different than 200 signals an error
+        else {
+            alert("There was a problem accessing the server: " +
+                xmlHttp.statusText);
+        }
+    }
+}
+// Map initialization
+var map = L.map('map', {
+    center: [0, 0],
+   
+}).setView([2.71638889, 9.86361111], 15);
+map.zoomControl.setPosition('topleft');
+map.options.minZoom = 14;
+map.on('click', placerMarqueur);
+// Osm Layer
+var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
+//osm.addTo(map);
+
+// Osm Hot Layer
+var osmHOT = L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap contributors, Tiles style by Humanitarian OpenStreetMap Team hosted by OpenStreetMap France'
+});
+var lat = 2.71638889, lon = 9.86361111;
+var marker = L.marker([2.71638889, 9.86361111]).addTo(map);
+
+//google street
+googleStreets = L.tileLayer('http://{s}.google.com/vt?lyrs=m&x={x}&y={y}&z={z}', {
+    maxZoom: 20,
+    subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
+});
+//googleStreets.addTo(map);
+
+
+//fonction pour cliquer
+function placerMarqueur(e) {
+    marker.setLatLng(e.latlng);
+    lat = e.latlng.lat;
+    lon = e.latlng.lng;
+
+};
+
+//google Sat
+googleSat = L.tileLayer('http://{s}.google.com/vt?lyrs=s&x={x}&y={y}&z={z}', {
+    maxZoom: 20,
+    subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
+});
+googleSat.addTo(map);
+
+//concession du port autonome de kribi
+
+
+// faire le tableau dans le popup
+
+
+
+
+
+
+
+
+//Layer controller
+
+var baseMaps = {
+    "OpenStreetMap": osm,
+    "OpenStreetMap.HOT": osmHOT,
+    "google Streets ": googleStreets,
+    " google Sat": googleSat
+};
+
+var overlayMaps = {
+    "concessions du P.A.K.": concessdata
+};
+
+var layerControl = L.control.layers(baseMaps, overlayMaps).addTo(map);
+
+
+
+L.control.coordinates({
+    position: "bottomleft",
+    useDMS: true,
+    decimals: 2,
+    decimalSeperator: ",",
+    labelTemplateLat: "Latitude {y}",
+    labelTemplateLng: "Longitude {x}",
 }).addTo(map);
 
-var marker = L.marker([lat, long]).addTo(map)
-    .bindPopup(`Votre position actuelle`)
-    .openPopup();
 
-marker.on('click', (e) => {
-    marker.bindPopup(`
-        <h4>Image Title</h4>
-        <table>
-            <tr>
-                <td>Propriété</td>
-                <td>Valeur</td>
-            </tr>
-            <tr>
-                <td>Propriété 1</td>
-                <td>Valeur 1</td>
-            </tr>
-            <tr>
-                <td>Propriété 2</td>
-                <td>Valeur 2</td>
-            </tr>
-        </table>
-        <img src="img/Dock.jpg" class="popup-image">
-    `);
-});
+// scale
 
- // draw geometry
+L.control.scale().addTo(map);
 
- var drawnFeatures = new L.FeatureGroup();
- map.addLayer(drawnFeatures);
+// draw geometry
+/*
+    var drawnFeatures = new L.FeatureGroup();
+    map.addLayer(drawnFeatures);
 
- var drawControl = new L.Control.Draw({
-     edit: { 
-         featureGroup: drawnFeatures,
-         remove: false
-     }
- });
- map.addControl(drawControl);
+    var drawControl = new L.Control.Draw({
+        edit: { 
+            featureGroup: drawnFeatures,
+            remove: false
+        }
+    });
+    map.addControl(drawControl);
 
- map.on("draw:created", function (e) {
-     console.log(e)
-     var type = e.layerType;
-     var layer = e.layer;
-     drawnFeatures.addLayer(layer);
- });
+    map.on("draw:created", function (e) {
+        console.log(e)
+        var type = e.layerType;
+        var layer = e.layer;
+        drawnFeatures.addLayer(layer);
+    });
 
- map.on("draw:edited", function (e) {
-     console.log(e)
-     var type = e.layerType;
-     var layers = e.layer;
-     
-     layers.eachLayer(function(layer){
-         console.log(layer)
-     })
- });
+    map.on("draw:edited", function (e) {
+        console.log(e)
+        var type = e.layerType;
+        var layers = e.layer;
+        
+        layers.eachLayer(function(layer){
+            console.log(layer)
+        })
+    });
+
+    */
+
+// Leaflet Measure
+
+L.control.measure({
+    primaryLengthUnit: 'kilometers',
+    secondaryLengthUnit: 'meter',
+    primaryAreaUnit: 'sqmeters',
+    secondaryAreaUnit: undefined
+}).addTo(map);
+
+// geolocation
+var mb = 12
+L.control.locate().addTo(map);
+
+
+
+
+
+
+
+
+
+
